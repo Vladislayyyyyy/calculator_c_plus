c759fa97c6057667b5293294c7ba16946c2c7354
Vlads <87931410+Vladislayyyyyy@users.noreply.github.com>
2024-02-19 20:15:42 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+using namespace std;
+int main(){
+    double first;
+    double second;
+    char operation;
+    cout << "Enter first number:" << endl;
+    cin >> first;
+    cout << "Pick mathmatics operation (+,-,*,/)" << endl;
+    cin >> operation;
+    cout << "Enter second number:" << endl;
+    cin >> second;
+    switch (operation) {
+        case '+':
+            cout << first + second;
+            break;
+        case '-':
+            cout << first-second;
+            break;
+        case '*':
+            cout << first*second;
+            break;
+        case '/':
+            cout << first/second;
+            break;
+return 0;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    }
 }
